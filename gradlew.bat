a774ae69956f83d0d8a4bd029169962722fb93e0
programmertwin <programmertwin@gmail.com>
2025-10-30 06:29:19 -0700

    Update gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,8 @@
+@rem
+@rem گرادل رپر برای ویندوز
+@rem
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
